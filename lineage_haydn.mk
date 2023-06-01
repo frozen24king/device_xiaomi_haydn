911006d5f2e9b559a5516b43951dac0432c94422
Astridxx <muratkozan350@gmail.com>
2023-06-01 16:25:43 +0300

    haydn: AlphaDroid-ify

--- lineage_haydn.mk
+++ lineage_haydn.mk
@@ -14,6 +14,27 @@ $(call inherit-product, device/xiaomi/haydn/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# GAPPS config
+# 0 - NO GAPPS (default)
+# 1 - CORE GAPPS
+# 2 - FULL GAPPS
+WITH_GAPPS := 2
+
+# Use Google Telephony pack (Dialer, Contacts, Messaging) on GAPPS builds
+# Default = true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+# Include Pixel Framework on FULL GAPPS builds
+# Default = true
+TARGET_USE_PIXEL_FRAMEWORK := true
+
+# Alpha prop
+ALPHA_BUILD_TYPE := unofficial
+ALPHA_MAINTAINER := frozenking
+
+# Set Boot Animination Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := haydn
 PRODUCT_MANUFACTURER := Xiaomi
@@ -22,6 +43,18 @@ PRODUCT_NAME := lineage_haydn
 
 PRODUCT_SYSTEM_NAME := haydn_global
 PRODUCT_SYSTEM_DEVICE := haydn
+
+
+# Alpha internal properties
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := true
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="haydn_global-user 13 TKQ1.220829.002 V14.0.5.0.TKKMIXM release-keys" \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,39 @@
 echo 'Starting to clone stuffs needed to build for Haydn'
 
+# Device common
+echo 'Cloning common device tree'
+
+git clone https://github.com/frozen24king/device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common
+
+# Kernel
+echo 'Cloning kernel tree'
+git clone --depth=1 --recursive https://github.com/frozen24king/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
+
+# Vendor
+echo 'Cloning vendor tree'
+git clone https://github.com/frozen24king/vendor_xiaomi_haydn -b lineage-20 vendor/xiaomi/haydn
+
+# Vendor common
+echo 'Cloning common vendor tree'
+git clone https://github.com/frozen24king/vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
+
+# Xiaomi
+echo 'Cloning hardware xiaomi'
+rm -rf hardware/xiaomi && git clone https://github.com/frozen24king/lineage_android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
+
 # Firmware
 echo 'Cloning firmware'
-git clone https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
+git clone https://gitlab.com/haydn_new/vendor_xiaomi_haydn-firmware.git -b thirteen vendor/xiaomi/haydn-firmware
 
 # Camera
 echo 'Cloning Leica Camera'
-git clone https://gitlab.com/Alucard_Storm/haydn-miuicamera -b thirteen-leica vendor/xiaomi/haydn-miuicamera
+git clone https://gitlab.com/haydn_new/haydn-miuicamera -b thirteen-leica vendor/xiaomi/haydn-miuicamera
+
 
 echo 'Adding slow-mo fix for Leica Camera'
 cd hardware/interfaces
-wget https://github.com/Astridxx/android_hardware_interfaces/commit/ca6551863fb82110688452981572d44a26806681.patch
-patch -p1 <ca6551863fb82110688452981572d44a26806681.patch
+wget https://raw.githubusercontent.com/frozen24king/Patch-haydn/Octavi-13/0003-Update-HIDL-overrideFormat-from-HAL.patch
+patch -p1 <0003-Update-HIDL-overrideFormat-from-HAL.patch
 cd ../..
 
 echo 'delete vendorsetup.sh from device tree once this is done'
